3cf82b55f27fcfd0ad341c6a0907dd3f2612a981
sushilkumbhar77@gmail.com <sushilkumbhar77@gmail.com>
2022-05-25 17:02:44 +0530

    color picker

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-setInterval(()=> {
+setInterval(() => {
   const colorName = document.getElementById("color").value;
   const colorValue = document.getElementById("colorValue");
   colorValue.innerText = colorName;
   colorValue.style.color = colorName;
-},1)
+}, 1);
+console.log("welcome to Colour Picker App.");
